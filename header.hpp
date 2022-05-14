d2a49ca076ac3795b3de1d3f99001955a67f0944
mamali543 <amalyreda@gmail.com>
2022-05-14 01:13:19 +0100

    Element access

--- header.hpp
+++ header.hpp
@@ -2,6 +2,8 @@
 #include <array>
 #include <memory>
 
+namespace ft
+{
 template < class T, class Alloc = std::allocator<T> >
 class vector
 {
@@ -14,33 +16,39 @@ class vector
     typedef typename __alloc_traits::const_pointer   	const_pointer;
     typedef std::size_t 								size_type;
     private:
-    size_t var;
-    value_type *p;
-    allocator_type allocc;
+        size_type value;
+        size_type size;
+        value_type *p;
+        allocator_type allocc;
     public:
-    explicit vector (const allocator_type& alloc = allocator_type()): var(0), p(NULL), allocc(alloc)
+    explicit vector (const allocator_type& alloc = allocator_type()): value(0), p(NULL), size(0)
     {
 
-    } 
-    explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()): var(n), p(NULL), allocc(alloc)
+    }
+
+    explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()): size(n), p(NULL), allocc(alloc)
     {
-        p = allocc.allocator(n);
-        for (int i = 0; i < n; i++)
+        size_type i;
+        p = allocc.allocate(n);
+        for (i = 0; i < n; i++)
         {
             p.construct(&p[i], val);
         }
     }
-    Vector (const Vector& x) : var(0), p(NULl), allocc(x.allocc)
+
+    Vector (const Vector& x) : p(NULl), allocc(x.allocc)
 	{
 		*this = x;
 	}
+    
     vector& operator= (const vector& x)
     {
-        if (var != 0)
-            allocc.deallocate(p, var);
-        var = x.var;
-        p = allocc.allocator(var);
-        for (int i = 0; i < var; i++)
+        size_type i;
+        if (size != 0)
+            allocc.deallocate(p, size);
+        size = x.size;
+        p = allocc.allocate(size);
+        for (i = 0; i < size; i++)
         {
             p.construct(&p[i], x[i]);
         }
@@ -49,14 +57,84 @@ class vector
 
     ~vector()
     {
-        if (var != 0)
-            allocc.deallocate(p, var);
+        if (size != 0)
+            allocc.deallocate(p, size);
     }
 
     size_type size() const
     {
-        
+        retunr (size);
     }
+
+    size_type max_size() const
+	{
+		return (allocc.max_size());
+	}
+
+    size_type capacity() const
+    {
+        return (size);
+    }
+    bool empty() const
+	{
+		if (size == 0)
+			return (1);
+		else
+			return 0;
+	}
+    void reserve (size_type n)
+    {
+        value_type *tmp;
+        size_type   i;
+        tmp = p
+        if (n > size)
+        {
+            p = allocc.allocate(p, n);
+            for (i = 0; i < n; i++)
+            {
+                allocc.construct(&p[i], tmp[i]);
+            }
+            if (size > 0)
+                allocc.deallocate(tmp, size);
+            size = n;
+        }
+    }
+
+    void resize (size_type n, value_type val = value_type())
+    {
+        size_type   i;
+        value_type  *tmp;
+        tmp = p
+        if (n < size)
+        {
+            for (i = 0; i < n ; i++)
+            {
+                allocc.construct(&p[i], val);
+            }
+        }
+        else if (n > 0)
+        {
+            p = allocc.allocate(n);
+            for (i = 0; i < n; i++)
+            {
+                if (i < size)
+                {
+                    allocc.construct(&p[i], tmp[i]);
+                }
+                else
+                    allocc.construct(&p[i], val);
+            }
+            if (size > 0)
+                allocc.deallocate(tmp, size);
+            size = n;
+        }
+        else
+            p = NULL;
+        value = n;
+    }
+
+
 };
+}
 
 

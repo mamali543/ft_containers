d059f483847bfd37f8767be58e563681b5e8d8e1
mamali543 <amalyreda@gmail.com>
2022-05-15 05:25:02 +0100

    modifiers

--- header.hpp
+++ header.hpp
@@ -21,6 +21,14 @@ class vector
         value_type *p;
         allocator_type allocc;
     public:
+
+    class outofbounds : public std::exception
+    {   public : 
+            const char* what() const throw()
+            {
+                return("index outofbounds");
+            }
+    };
     explicit vector (const allocator_type& alloc = allocator_type()): value(0), p(NULL), size(0)
     {
 
@@ -75,6 +83,7 @@ class vector
     {
         return (size);
     }
+
     bool empty() const
 	{
 		if (size == 0)
@@ -82,6 +91,7 @@ class vector
 		else
 			return 0;
 	}
+
     void reserve (size_type n)
     {
         value_type *tmp;
@@ -132,7 +142,51 @@ class vector
             p = NULL;
         value = n;
     }
+/*------------------Element access-------------------------*/
+    reference at (size_type n)
+    {
+        if (n < size && n >= 0)
+            return (p[n]);
+        throw outofbounds();
 
+    }
+
+    const_reference at (size_type _n) const
+	{
+		if (_n < size)
+			return (p[n]);
+		throw outofbounds();
+	}
+
+    reference operator[] (size_type n)
+    {
+        return (p[n]);
+    }
+
+    const_reference operator[] (size_type n) const
+    {
+        return (p[n]);
+    }
+
+    reference front()
+    {
+        return (p[0]);
+    }
+    const_reference front() const
+    {
+        return (p[0]);
+    }
+
+    reference back()
+    {
+        return (p[size - 1]);
+    }
+
+    const_reference back() const
+    {
+        return (p[size - 1]);
+    }
+/*------------------Modifiers-------------------------*/
 
 };
 }
